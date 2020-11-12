661c23884f9ff41df92a8e450380c10a0d497b4d
Leeaerisa <73582933+Leeaerisa@users.noreply.github.com>
2020-11-12 18:34:38 +0900

    boot06
    Spring boot06 프로젝트

---
+++ boot06/src/main/java/com/edu/persistence/WebBoardRepository.java
@@ -0,0 +1,50 @@
+package com.edu.persistence;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import com.edu.domain.QWebBoard;
+import com.edu.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+
+// QuerydslPredicateExecutor<WebBoard>
+// 검색 기능을 구현할 때 사용할 수 있도록 WebBoardRepository에 Querydsl 관련 인터페이스를 추가한다.
+public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard>{
+
+	public default Predicate makePredicate(String type, String keyword){
+		
+		BooleanBuilder builder = new BooleanBuilder();
+		
+		QWebBoard board = QWebBoard.webBoard;
+		
+		builder.and(board.bno.gt(0));	//board.bno > 0
+		
+		if(type == null) {
+			return builder;
+		}
+		
+		// 검색 조건을 선택하여 검색한다.
+		switch(type) {
+			case "t":
+				//select * from board where bno > 0 and title "%keyworld%";
+				builder.and(board.title.like("%" + keyword +"%"));
+				break;
+			case "c":
+				builder.and(board.content.like("%" + keyword +"%"));
+				break;
+			case "w":
+				builder.and(board.writer.like("%" + keyword +"%"));
+				break;
+		}
+		
+		return builder;
+	}
+	
+
+}
+
+
+
+
+
